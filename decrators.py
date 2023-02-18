3b8048cfc459805414e63e973bcaef7f67e7c5e8
koltikin <koltikin210@gmail.com>
2023-02-18 10:47:22 +0300

    some changes

--- decrators.py
+++ decrators.py
@@ -1,42 +1,49 @@
 # function based decrator__________________
 
 
-def decrator(decrated_function):
-    def wrapper_fun(*args, **kwargs):
-        print(f"decrator excut this befor {decrated_function.__name__}")
-        decrated_function(*args, **kwargs)
-        print("after decrated_fun")
+# def decrator(decrated_function):
+#     def wrapper_fun(*args):
+#         print(f"decrator excut this befor {decrated_function.__name__}")
+#         decrated_function(args[0], args[1])
+#         print(args[2])
+#         print("after decrated_fun")
 
-    return wrapper_fun
-
-
-@decrator
-def decrated_function(name, age):
-    print(f"my name is {name}")
-    print(f"my age is {age}")
+#     return wrapper_fun
 
 
-decrated_function("ziya", 32)
+# @decrator
+# def decrated_function(name, age):
+#     print(f"my name is {name}")
+#     print(f"my age is {age}")
+
+
+# decrated_function("ziya", 32, "ziya kasgari")
 
 # class based decrator__________________
 
 
 class decrator:
-    def __init__(self, decrated_function):
-        self.decrated_function = decrated_function
+    def __init__(self, b):
+        # pass
+        # decrated_function.__init__(self,naem,age)
+        print(b)
+        self.b = b
+        # self.decrated_function = decrated_function
+        # self.full_name = full_name
+        # self.full_name = full_name
 
     def __call__(self, *args, **kwargs):
-        print(f"decrator excut thi befor {self.decrated_function.__name__}")
+        # print(f"decrator excut this befor {decrated_function.__name__()}")
         print("after decrated_fun")
-        pp = self.decrated_function(*args, **kwargs)
-        return pp
+        print(self.b)
+        return "hello"
 
 
 @decrator
 class decrated_function:
     def __init__(self, name, age):
         print(f"my name is {name}")
-        print(f"my age is {age}")
+        # print(f"my age is {age}")
         self.name = name
         self.age = age
 
@@ -44,59 +51,61 @@ class decrated_function:
         print(f"my name is {self.name}, my age is {self.age}")
 
 
-# pp = decrated_function("ziya", 32)
+pp = decrated_function("ziya")
+print(pp)
 # pp.see_togather()
 
 # decrate class with function __________________
 
 
-def decrator(fun):
-    def wraper(*args, **kwargs):
-        print(f"decrator excut thi befor {fun.__name__}")
-        print("after decrated_fun")
-        gg = fun(*args, **kwargs)
-        return gg
+# def decrator(fun):
+#     def wraper(*args, **kwargs):
+#         print(f"decrator excut thi befor {fun.__name__}")
+#         print("after decrated_fun")
+#         gg = fun(*args, **kwargs)
+#         return gg
 
-    return wraper
-
-
-@decrator
-class decrated_function:
-    def __init__(self, name, age):
-        print(f"my name is {name}")
-        print(f"my age is {age}")
-        self.name = name
-        self.age = age
-
-    def see_togather(self):
-        print(f"my name is {self.name}, my age is {self.age}")
+#     return wraper
 
 
-# pp = decrated_function("ziya", 32)
-# pp.see_togather()
+# @decrator
+# class decrated_function:
+#     def __init__(self, name, age):
+#         print(f"my name is {name}")
+#         print(f"my age is {age}")
+#         self.name = name
+#         self.age = age
+
+#     def see_togather(self):
+#         print(f"my name is {self.name}, my age is {self.age}")
 
 
-# decrate function with class __________________
+# # pp = decrated_function("ziya", 32)
+# # pp.see_togather()
 
 
-class decrator:
-    def __init__(self, fun):
-        self.fun = fun
-        print(f"decrator excut thi befor {self.fun.__name__}")
-        print("after decrated_fun")
-
-    def __call__(self, *args, **kwds):
-        print("hello")
-        self.fun(*args, **kwds)
-        print("after hello")
+# # decrate function with class __________________
 
 
-@decrator
-def decrated_function(name, age):
-    print(f"my name is {name}")
-    print(f"my age is {age}")
-    print(6 + 8)
-    print(f"my name is {name}, my age is {age}")
+# class decrator:
+#     def __init__(self, fun):
+#         pass
+#         # self.fun = fun
+#         # print(f"decrator excut thi befor {self.fun.__name__}")
+#         # print("after decrated_fun")
+
+#     def __call__(self, *args, **kwds):
+#         print("hello")
+#         self.fun(*args, **kwds)
+#         print("after hello")
 
 
-# decrated_function("ziya", 32)
+# @decrator
+# def decrated_function(name, age):
+#     print(f"my name is {name}")
+#     print(f"my age is {age}")
+#     print(6 + 8)
+#     print(f"my name is {name}, my age is {age}")
+
+
+# # decrated_function("ziya", 32)
--- oop variables.py
+++ oop variables.py
@@ -1,23 +1,40 @@
 class Employe(object):
     num_of_Employe = 0
     raise_amount = 1.04
+
     def __init__(self, frist, last, pay):
         self.frist_name = frist
         self.last_name = last
-        self.email = '{}.{}@gmail.com'.format(frist,last)
-        self.pay = pay
-        Employe.num_of_Employe +=1
+        self.email = '{}.{}@gmail.com'.format(frist, last)
+        self._pay = pay
+        Employe.num_of_Employe += 1
 
     def apply_raise(self):
-            self.pay = int(self.pay*self.raise_amount)
+        self.pay = int(self.pay * self.raise_amount)
 
-em_1 = Employe("ziya","kasgari",5200)
-em_2 = Employe("ablet","atus",7800)
-em_3 = Employe("ablet","atus",7800)
-em_4 = Employe("ablet","atus",7800)
+    @property
+    def pay(self):
+        return self._pay
+
+    @pay.setter
+    def pay(self, new_pay):
+        self._pay = new_pay
+
+    @pay.deleter
+    def pay(self):
+        del self.pay
+
+
+em_1 = Employe("ziya", "kasgari", 5200)
+em_2 = Employe("ablet", "atus", 7800)
+em_3 = Employe("ablet", "atus", 7800)
+em_4 = Employe("ablet", "atus", 7800)
 # print(Employe.__dict__)
 # print(em_1.__dict__)
-em_1.apply_raise()
-em_2.apply_raise()
-print(Employe.num_of_Employe)
-print(em_2.pay)
+# em_1.apply_raise()
+# em_2.apply_raise()
+# print(Employe.num_of_Employe)
+# print(em_2.pay)
+# del em_4
+# print(em_2.num_of_Employe)
+# print(em_3.num_of_Employe)
